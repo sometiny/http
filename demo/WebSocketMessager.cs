da1491db4cbe80bd32bf73c6ea466802dc17c86a
anlige <zhanghuiguoanlige@com>
2021-09-11 15:21:09 +0800

    示例代码；

--- demo/WebSocketMessager.cs
+++ demo/WebSocketMessager.cs
@@ -32,36 +32,77 @@ namespace IocpSharp.Http
     public class WsMessager : Messager
     {
         private EndPoint _remoteEndPoint = null;
-        public WsMessager(Stream stream) : base(stream) {
-            if(stream is BufferedNetworkStream networkStream)
+        public WsMessager(Stream stream) : base(stream)
+        {
+
+            //获取客户端的连接信息
+            if (stream is BufferedNetworkStream networkStream)
             {
-                _remoteEndPoint = networkStream.BaseSocket.RemoteEndPoint ;
+                _remoteEndPoint = networkStream.BaseSocket.RemoteEndPoint;
             }
         }
 
+        /// <summary>
+        /// 客户端新连接
+        /// </summary>
         protected override void OnConnected()
         {
-            Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 新客户端连接：{_remoteEndPoint}"  );
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 新客户端连接：{_remoteEndPoint}");
         }
+
+        /// <summary>
+        /// 客户端断开连接
+        /// </summary>
         protected override void OnDisconnected()
         {
             Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 连接断开：{_remoteEndPoint}");
         }
 
-        protected override void OnText(string payload)
+        /// <summary>
+        /// 接收到新的帧，仅展示下
+        /// </summary>
+        /// <param name="frame"></param>
+        protected override void OnNewFrame(Frame frame)
         {
-            Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 接收到文本数据：{payload}");
-            Send($"服务器接收到文本数据：{payload}");
-            if(payload == "close")
-            {
-                Close();
-            }
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 接收到新帧；帧类型：{frame.OpCode}，结束帧：{frame.Fin}，携带掩码：{frame.Mask}，长度：{frame.PayloadLength}");
+            Console.ForegroundColor = color;
         }
 
+        /// <summary>
+        /// 收到Text消息时的实现
+        /// 里面定义两个特使的消息：close和ping，用来测试服务器主动发送Close和Ping帧。
+        /// </summary>
+        /// <param name="payload">完整消息</param>
+        protected override void OnText(string payload)
+        {
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 文本数据：{payload}");
+            if (payload == "close")
+            {
+                Send($"服务器接收到close指令，关闭连接。");
+                Close();
+                return;
+            }
+            if (payload == "ping")
+            {
+                Send($"服务器接收到ping指令，发送ping。");
+                Ping();
+                return;
+            }
+            Send($"服务器接收到文本数据：{payload}");
+        }
+
+        /// <summary>
+        /// 接收到二进制消息
+        /// 二进制消息是通过流来读取的，不像Text，直接一股脑读取全部消息。
+        /// </summary>
+        /// <param name="inputStream">输入流</param>
         protected override void OnBinary(Stream inputStream)
         {
+            //为了测试，我们把二进制消息读取到字节数组。
             byte[] payload = StreamUtils.ReadAllBytes(inputStream);
-            Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 接收到二进制数据，长度：{payload.Length}");
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 二进制数据，长度：{payload.Length}");
             Send($"服务器接收到二进制数据，长度：{payload.Length}");
         }
     }
--- src/HttpServer.cs
+++ src/HttpServer.cs
@@ -25,28 +25,46 @@ namespace IocpSharp.Http
         }
         protected override void OnWebSocket(HttpRequest request, Stream stream)
         {
+            //使用自己实现的Messager类处理业务。
             new MyMessager(stream).Accept();
         }
     }
 
+    /// <summary>
+    /// 实现一个Messager类
+    /// </summary>
     public class MyMessager : Messager
     {
         private EndPoint _remoteEndPoint = null;
         public MyMessager(Stream stream) : base(stream) {
+
+            //获取客户端的连接信息
             if(stream is BufferedNetworkStream networkStream)
             {
                 _remoteEndPoint = networkStream.BaseSocket.RemoteEndPoint ;
             }
         }
 
+        /// <summary>
+        /// 客户端新连接
+        /// </summary>
         protected override void OnConnected()
         {
             Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 新客户端连接：{_remoteEndPoint}");
         }
+
+        /// <summary>
+        /// 客户端断开连接
+        /// </summary>
         protected override void OnDisconnected()
         {
             Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 连接断开：{_remoteEndPoint}");
         }
+
+        /// <summary>
+        /// 接收到新的帧，仅展示下
+        /// </summary>
+        /// <param name="frame"></param>
         protected override void OnNewFrame(Frame frame)
         {
             var color = Console.ForegroundColor;
@@ -55,6 +73,11 @@ namespace IocpSharp.Http
             Console.ForegroundColor = color;
         }
 
+        /// <summary>
+        /// 收到Text消息时的实现
+        /// 里面定义两个特使的消息：close和ping，用来测试服务器主动发送Close和Ping帧。
+        /// </summary>
+        /// <param name="payload">完整消息</param>
         protected override void OnText(string payload)
         {
             Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 文本数据：{payload}");
@@ -73,8 +96,14 @@ namespace IocpSharp.Http
             Send($"服务器接收到文本数据：{payload}");
         }
 
+        /// <summary>
+        /// 接收到二进制消息
+        /// 二进制消息是通过流来读取的，不像Text，直接一股脑读取全部消息。
+        /// </summary>
+        /// <param name="inputStream">输入流</param>
         protected override void OnBinary(Stream inputStream)
         {
+            //为了测试，我们把二进制消息读取到字节数组。
             byte[] payload = StreamUtils.ReadAllBytes(inputStream);
             Console.WriteLine($"{DateTime.Now:HH:mm:ss} > 二进制数据，长度：{payload.Length}");
             Send($"服务器接收到二进制数据，长度：{payload.Length}");
