8c1a953398b3128fd2ee6eea67f5ebb86c8b9aab
anlige <zhanghuiguoanlige@126.com>
2021-09-13 14:59:09 +0800

    静态文件服务器Demo；

--- Program.cs
+++ Program.cs
@@ -10,17 +10,38 @@ namespace IocpSharp.Http
     {
         static void Main(string[] args)
         {
+            StartWebSocketServer(4189);
+
+            Console.ReadLine();
+        }
+
+        private static void StartWebSocketServer(int port)
+        {
             HttpServerBase server = new WebSocketMessager();
             try
             {
-                server.Start("0.0.0.0", 4189);
-                Console.WriteLine("服务器启动成功，监听地址：" + server.LocalEndPoint.ToString());
-            }catch(Exception e)
+                server.Start("0.0.0.0", port);
+                Console.WriteLine("WebSocket服务器启动成功，监听地址：" + server.LocalEndPoint.ToString());
+            }
+            catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
-
-            Console.ReadLine();
+        }
+        private static void StartStaticServer(int port)
+        {
+            //静态文件服务器
+            HttpServerBase server = new StaticHttpServer();
+            try
+            {
+                server.Start("0.0.0.0", port);
+                Console.WriteLine("静态HTTP服务器启动成功，监听地址：" + server.LocalEndPoint.ToString());
+                Console.WriteLine($"HTTP服务器根目录：{server.WebRoot}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
---
+++ demo/StaticHttpServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using IocpSharp.Http.Responsers;
+using IocpSharp.Http.Streams;
+using System.IO.Compression;
+using IocpSharp.Server;
+using IocpSharp.Http.Utils;
+using IocpSharp.WebSocket;
+using IocpSharp.WebSocket.Frames;
+using System.Net;
+
+namespace IocpSharp.Http
+{
+    /// <summary>
+    /// 静态文件服务，通过WebRoot设置根目录
+    /// </summary>
+    public class StaticHttpServer : HttpServerBase
+    {
+        public StaticHttpServer() : base()
+        {
+            //设置根目录
+            WebRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "web"));
+        }
+    }
+}
